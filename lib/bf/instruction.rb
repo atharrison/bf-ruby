fdcaeb241c6515e554f8e5873c3a1f021e04be8c
Andrew Harrison <andrewharrison@otherinbox.com>
2014-08-11 16:58:08 -0500

    More Instruction refactor

--- lib/bf/instruction.rb
+++ lib/bf/instruction.rb
@@ -1,23 +1,31 @@
 module BF
   module Instruction
 
+    DECREMENT = Instruction::Decrement.new
+    INCREMENT = Instruction::Increment.new
+    INPUT_CHARACTER = Instruction::InputCharacter.new
+    JUMP = Instruction::Jump.new
+    JUMP_RETURN = Instruction::JumpReturn.new
+    MOVE_LEFT = Instruction::MoveLeft.new
+    MOVE_RIGHT = Instruction::MoveRight.new
+    OUTPUT_CHARACTER = Instruction::OutputCharacter.new
+
+
     def self.act(command, program)
       return if instructions[command].nil?
-      #next_ptr = instructions[command].call(program)
-      #binding.pry if next_ptr.nil?
       instructions[command].call(program)
     end
 
     def self.instructions
       {
-          '>' => Instruction::MoveRight.new,
-          '<' => Instruction::MoveLeft.new,
-          '+' => Instruction::Increment.new,
-          '-' => Instruction::Decrement.new,
-          '.' => Instruction::OutputCharacter.new,
-          ',' => Instruction::InputCharacter.new,
-          '[' => Instruction::Jump.new,
-          ']' => Instruction::JumpReturn.new,
+          '>' => MOVE_RIGHT,
+          '<' => MOVE_LEFT,
+          '+' => INCREMENT,
+          '-' => DECREMENT,
+          '.' => OUTPUT_CHARACTER,
+          ',' => INPUT_CHARACTER,
+          '[' => JUMP,
+          ']' => JUMP_RETURN,
       }
     end
   #
--- lib/bf/instruction/input_character.rb
+++ lib/bf/instruction/input_character.rb
@@ -2,21 +2,16 @@ module BF
   module Instruction
     class InputCharacter
 
-      #attr_reader :input_tokens
-
       def initialize
         @input_tokens = []
       end
 
       def call(program)
-        program.read_input
-      #  if @input_tokens.length == 0
-      #    @input_tokens = STDIN.readline.scan(/./).push("\n")
-      #    #binding.pry
-      #    #puts @input_tokens
-      #  end
-      #  #puts @input_tokens
-      #  program.data[program.data_ptr] = @input_tokens.shift
+        if @input_tokens.length == 0
+          @input_tokens = STDIN.readline.scan(/./).push("\n")
+        end
+        program.data[program.data_ptr] = @input_tokens.shift
+
         program.cmd_ptr + 1
       end
     end
